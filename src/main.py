18979a40c157c4403a6e1c0cfde1c4121b85f65f
torstein <668523@stud.hvl.no>
2026-02-10 19:23:36 +0100

    Utvidet main og index + lagd mainpage

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app  = Flask(__name__)
-@app.route('/')
-def index():
-    mountains = ['Everest', 'K2', 'Galhogpiggen']
-    return render_template('index.html', mountain=mountains)
 
-@app.route('/mountain/<mt>')
-def mountain(mt):
-    return "This is " + str(mt)
+#Variables
+VALID_USERNAME = 'test'
+VALID_PASSWORD = 'test'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == VALID_USERNAME and password == VALID_PASSWORD:
+            return jsonify({"success": True, "message": "Login successful"})
+        else :
+            return jsonify({"success": False, "message": "Username and password do not match"})
+
+    return render_template('index.html')
+
+@app.route('/mainpage')
+def mainpage():
+    return render_template('mainpage.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
