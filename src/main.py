f90a8dcf65c5694b3fad90b9521d8d52de8c575d
h668523 <111427648+h668523@users.noreply.github.com>
2026-02-10 15:21:40 +0100

    Restructure og html

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from flask import Flask, render_template
+
+app  = Flask(__name__)
+@app.route('/')
+def index():
+    mountains = ['Everest', 'K2', 'Galhogpiggen']
+    return render_template('index.html', mountain=mountains)
+
+@app.route('/mountain/<mt>')
+def mountain(mt):
+    return "This is " + str(mt)
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
