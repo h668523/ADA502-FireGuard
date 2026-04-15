8d2223d45f9758a01be3a8237a714a53bf6ed249
h668523 <tor-sle@hotmail.com>
2026-04-15 20:53:48 +0200

    testing

--- src/ada502_fireguard/main.py
+++ src/ada502_fireguard/main.py
@@ -394,6 +394,7 @@ def trigger_daily_task():
 # Database greier
 @app.route("/favorite/<string:tettsted_name>/<string:kommune_name>/<string:fylke_name>", methods=["POST"])
 def add_favorite(tettsted_name, kommune_name, fylke_name):
+    print(session)
     print(fylke_name,tettsted_name,kommune_name)
     userinfo = session.get("user")
     if not userinfo:
